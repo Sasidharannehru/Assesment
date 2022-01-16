cde53a06b518a9ae05a44116e4fbce6656680038
ELCOT <ELCOT@Lenovo>
2022-01-16 12:03:09 +0530

    second commit

---
+++ phase1Project/src/com/sasi/file/handling/MainMethod.java
@@ -0,0 +1,17 @@
+package com.sasi.file.handling;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		
+			
+			// Create "main" folder if not present in current folder structure
+			FileOperation.createMainFolderIfNotPresent("main");
+			
+			MenuOptions.printWelcomeScreen("This application", "Sasidharan");
+			
+			HandleOptions.handleWelcomeScreenInput();
+
+	}
+
+}
